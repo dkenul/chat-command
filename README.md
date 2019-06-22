a8e5a401b1c7c199b01bf142e3b90cdc4fb16e60
Damon <damon.kenul@gmail.com>
2019-06-22 15:00:35 -0400

    init module

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chat Command
+
+## This is a PoC
+
+So use at your own risk
+
+## Intention
+Parse namespaced command(s) from text. Intended to be consumed by a chat bot.
---
+++ index.js
@@ -0,0 +1,41 @@
+const isPromise = p => p instanceof Promise
+const isString = s => typeof s === 'string'
+const isFunction = f => typeof f === 'function'
+
+function chatCommand (namespace, actions) {
+  const parse = text => {
+    const commands = []
+    text.replace(new RegExp(`(?:${namespace}\\.)([^\\n ]*)`, 'gm'), (_, cmd) => {
+      commands.push(cmd)
+    })
+    return commands
+  }
+  const execute = command => command
+    .split('.')
+    .reduce((context, subcommand) => {
+      if (isString(context) || isPromise(context)) return context
+
+      if (subcommand.includes('(')) {
+        const arg = subcommand.replace(/^.*\(([^()]*)\).*$/, '$1')
+        const action = context[subcommand.split('(')[0]]
+
+        return action(arg)
+      } else {
+        const action = context[subcommand]
+
+        return isFunction(action) ? action() : action
+      }
+    }, actions)
+
+  const executeAll = commands => Promise.all(commands.map(execute))
+  const parseAndExecuteAll = text => executeAll(parse(text))
+
+  return {
+    parse,
+    execute,
+    executeAll,
+    parseAndExecuteAll,
+  }
+}
+
+module.exports = chatCommand
